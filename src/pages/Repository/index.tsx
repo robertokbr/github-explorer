0703df343d31c52dcf031232c280b3c6e5dd986b
Roberto Junior <robertojr-13-@hotmail.com>
2020-07-10 12:32:36 -0300

    Add second page style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
 interface RepositoryParams {
   repository: string;
@@ -7,10 +11,58 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+  const [repository, setRepository] = useState();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(ressponse => {
+      console.log();
+    });
+  }, []);
   return (
-    <div className="container">
-      <h1>{params.repository}</h1>
-    </div>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/60328400?s=460&u=036f9101c10b989343b18b07677ab94ae748e032&v=4"
+            alt="Github Perfil"
+          />
+          <div>
+            <strong>Nome do programador</strong>
+            <p>Descrição do perfil</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>18808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>19</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>100</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="Repository issue">
+          <div>
+            <strong>Issue title</strong>
+            <p>Description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
